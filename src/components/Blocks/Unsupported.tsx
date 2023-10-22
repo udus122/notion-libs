acbd6a0675dd7dbdab81f64402fa0b974bcfdd72
yusuda <yusuda@yahoo-corp.jp>
2023-10-22 18:55:57 +0900

    chore: remove unnecessary codes

--- src/components/Blocks/Unsupported.tsx
+++
@@ -1,9 +0,0 @@
-import type { BlockObject } from "src/libs/notion/blocks/blocks.js";
-import type { BlockProps } from "src/types/utils.js";
-
-type Props = BlockProps<BlockObject>;
-
-export const Unsupported: React.FC<Props> = ({ block }) => {
-  console.warn(`${block.type} is not supported`);
-  return null;
-};
